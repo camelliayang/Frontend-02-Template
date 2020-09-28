b825ec973f76f6b1b031fdce244e5c86fb7e6410
yanglu <camelliadata@gmail.com>
2020-09-27 23:56:57 -0700

    Fix bug.

--- week13/JSX/animation-demo.js
+++ week13/JSX/animation-demo.js
@@ -5,7 +5,7 @@ let timeline = new Timeline();
 timeline.start();
 
 // timeline.add(new Animation(document.querySelector("#el").style, "transform", 0, 500, 2000, 0, null, v => `translateX(${v}px)`));
-tl.add(new Animation(document.getElementById("ani").style, "transform", 0, 500, 5000, 0, ease, v => `translateX(${v}px)`));
+tl.add(new Animation(document.querySelector("#el").style, "transform", 0, 500, 5000, 0, ease, v => `translateX(${v}px)`));
 
 document.querySelector("#el2").style.transition = "transform 5s ease";
 document.querySelector("#el2").style.transform = `translateX(500px)`;
